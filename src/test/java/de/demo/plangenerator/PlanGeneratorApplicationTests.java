0dfde627115408b8bde9f2ba07efe2b9044220b1
Pawel Wyrwinski <pawel.wyrwinski@outfittery.de>
2020-02-28 20:43:15 +0100

    Initial commit

---
+++ src/test/java/de/demo/plangenerator/PlanGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package de.demo.plangenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

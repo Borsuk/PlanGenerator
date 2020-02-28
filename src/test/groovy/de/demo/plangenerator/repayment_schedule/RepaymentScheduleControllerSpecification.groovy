d89c10a53e677b9d35e026e45226c1345096df31
Pawel Wyrwinski <pawel.wyrwinski@outfittery.de>
2020-02-28 20:49:42 +0100

    Controller tests - focus on validation

---
+++ src/test/groovy/de/demo/plangenerator/repayment_schedule/RepaymentScheduleControllerSpecification.groovy
@@ -0,0 +1,125 @@
+package de.demo.plangenerator.repayment_schedule
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MockMvc
+import spock.lang.Specification
+import spock.lang.Unroll
+
+import java.time.ZonedDateTime
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@WebMvcTest
+class RepaymentScheduleControllerSpecification extends Specification {
+
+    @Autowired
+    private MockMvc mockMvc
+
+    @Autowired
+    private ObjectMapper objectMapper
+
+    @Unroll
+    def 'request to generate Repayment Schedule fails with invalid loanAmount - #reasonToFail'(){
+        given: 'invalid request object without loanAmount'
+            GeneratePlanRequest request = new GeneratePlanRequest(
+                    loanAmount : loanAmount,
+                    nominalRate: BigDecimal.ONE,
+                    duration: 24,
+                    startDate: ZonedDateTime.now()
+            )
+        and: 'its text representation'
+            String requestWithoutLoanAmount = objectMapper.writeValueAsString(request)
+
+        expect:
+            mockMvc.perform(
+                    post('/generate-plan')
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(requestWithoutLoanAmount)
+            )
+                    .andExpect(status().isBadRequest())
+        where:
+            loanAmount                      |   reasonToFail
+            null                            |   'is null'
+            BigDecimal.valueOf(-1.0d)       |   'is negative'
+            BigDecimal.ZERO                 |   'is zero'
+    }
+
+    @Unroll
+    def 'request to generate Repayment Schedule fails with invalid nominalRate - #reasonToFail'(){
+        given: 'invalid request object without nominalRate'
+            GeneratePlanRequest request = new GeneratePlanRequest(
+                    loanAmount : BigDecimal.valueOf(1000.0d),
+                    nominalRate: nominalRate,
+                    duration: 24,
+                    startDate: ZonedDateTime.now()
+            )
+        and: 'its text representation'
+            String requestWithoutLoanAmount = objectMapper.writeValueAsString(request)
+
+        expect:
+            mockMvc.perform(
+                    post('/generate-plan')
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(requestWithoutLoanAmount)
+            )
+                    .andExpect(status().isBadRequest())
+        where:
+            nominalRate                     |   reasonToFail
+            null                            |   'is null'
+            BigDecimal.valueOf(-1.0d)       |   'is negative'
+            BigDecimal.valueOf(101.0d)      |   'it\'s over 100 %'  // my assumption is that the rate for the loan is invalid if > 100%
+    }
+
+    @Unroll
+    def 'request to generate Repayment Schedule fails with invalid duration - #reasonToFail'(){
+        given: 'invalid request object without duration'
+            GeneratePlanRequest request = new GeneratePlanRequest(
+                    loanAmount : BigDecimal.valueOf(1000.0d),
+                    nominalRate: BigDecimal.ONE,
+                    duration: duration,
+                    startDate: ZonedDateTime.now()
+            )
+        and: 'its text representation'
+            String requestWithoutLoanAmount = objectMapper.writeValueAsString(request)
+
+        expect:
+            mockMvc.perform(
+                    post('/generate-plan')
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(requestWithoutLoanAmount)
+            )
+                    .andExpect(status().isBadRequest())
+        where:
+            duration                        |   reasonToFail
+            null                            |   'is null'
+            BigDecimal.valueOf(-1.0d)       |   'is negative'
+            BigDecimal.ZERO                 |   'is zero duration'
+    }
+
+    def 'request to generate Repayment Schedule is invalid without startDate'(){
+        given: 'invalid request object without startDate'
+            GeneratePlanRequest request = new GeneratePlanRequest(
+                    loanAmount : BigDecimal.valueOf(1000.0d),
+                    nominalRate: BigDecimal.ONE,
+                    duration: 24,
+                    startDate: null
+            )
+        and: 'its text representation'
+            String requestWithoutLoanAmount = objectMapper.writeValueAsString(request)
+
+        expect:
+            mockMvc.perform(
+                    post('/generate-plan')
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(requestWithoutLoanAmount)
+            )
+                    .andExpect(status().isBadRequest())
+    }
+
+}

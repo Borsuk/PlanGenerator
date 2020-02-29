4ef6d66ad9d07cb7bba4e23068c2cc303361e4e7
Pawel Wyrwinski <pawel.wyrwinski@outfittery.de>
2020-02-29 20:33:29 +0100

    Combine all formulas together to compute all repayment installments

--- src/main/java/de/demo/plangenerator/repayment_schedule/RepaymentScheduleService.java
+++ src/main/java/de/demo/plangenerator/repayment_schedule/RepaymentScheduleService.java
@@ -1,9 +1,11 @@
 package de.demo.plangenerator.repayment_schedule;
 
 import de.demo.plangenerator.utils.FinCalc;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.time.Duration;
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -11,15 +13,39 @@ import java.util.List;
 @Service
 public class RepaymentScheduleService {
 
+    @Value("${repayment_schedule.days_in_month:30}")
+    Integer daysInMonth;
+
+    @Value("${repayment_schedule.days_in_year:360}")
+    Integer daysInYear;
+
     public List<InstallmentPlan> calculateRepaymentSchedule(
             BigDecimal loanAmount,
             BigDecimal annualNominalRate,
             Integer duration,
             ZonedDateTime startDate) {
 
-        BigDecimal annuity = FinCalc.annuity(annualNominalRate, loanAmount, duration);
+        List<InstallmentPlan> installments = new ArrayList<>(duration);
 
-        return new ArrayList<>();
+        BigDecimal remainingOutstandingPrincipal = loanAmount;
+
+        for (int installmentNo=0; installmentNo < duration; installmentNo++) {
+            BigDecimal initialOutstandingPrincipal = remainingOutstandingPrincipal;
+
+            BigDecimal annuity = FinCalc.annuity(annualNominalRate, loanAmount, duration);
+            BigDecimal interest = FinCalc.monthlyInterest(annualNominalRate, daysInMonth, daysInYear, initialOutstandingPrincipal);
+            BigDecimal principal = FinCalc.monthlyPrincipal(annuity, interest, initialOutstandingPrincipal);
+            BigDecimal borrowerPaymentAmount = FinCalc.borrowerPaymentAmount(principal, interest);
+            remainingOutstandingPrincipal = initialOutstandingPrincipal.subtract(principal);
+
+            //TODO prepare info on installment -- add it to the list
+            ZonedDateTime date = startDate.plus(Duration.ofDays(30 * installmentNo));
+            InstallmentPlan currentInstallment = new InstallmentPlan(date, borrowerPaymentAmount, principal, interest, initialOutstandingPrincipal, remainingOutstandingPrincipal);
+
+            installments.add(currentInstallment);
+        }
+
+        return installments;
     }
 
 
